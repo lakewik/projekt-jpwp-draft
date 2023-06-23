6ac01964f4dcc733110e488fe97fedc2715f1383
wikjezioro@op.pl <you@example.com>
2023-06-23 17:23:14 +0200

    Add first screen definition of frontend app, and initial front commit

---
+++ tasks-frontend/src/RegisterForm.js
@@ -0,0 +1,11 @@
+ 
+import { useState } from 'react';
+import api from './api';
+
+
+
+function RegisterForm() {
+
+}
+
+export default RegisterForm;
---
+++ tasks-frontend/src/TaskList.js
@@ -0,0 +1,9 @@
+ 
+import { useState, useEffect } from 'react';
+import api from './api';
+
+function TaskList() {
+ 
+}
+
+export default TaskList;
