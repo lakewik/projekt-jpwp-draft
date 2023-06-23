6d04f8c5098ba3f9bfcef86bbc976d6927f18c1d
wikjezioro@op.pl <you@example.com>
2023-06-23 14:13:51 +0200

    Add CORS allow all origins to avoid problems withj local development

--- API/server.js
+++ API/server.js
@@ -46,6 +46,13 @@ const taskSchema = new mongoose.Schema({
 const Task = mongoose.model('Task', taskSchema);
 
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 
